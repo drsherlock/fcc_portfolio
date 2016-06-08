7434bc09c725b6524c690d000723ed048682448a
Zain Ul Haq <zain.delhi27@hotmail.com>
2016-06-08 23:29:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-  
+$('a[href*=#]').on('click', function(event){     
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
+$(".work-image").hover(function () {
+  var sliderId= $(this).children('.slider').attr("id"); 
+  var width = $(".work-image img").width();
+  $('#'+sliderId).css("width", width);
+  $('#'+sliderId).slideToggle("slow");
 });
